64e7dd53f2f2819e1b60de6bf032becc363c5375
Alejandro Mallea <janoma@gmail.com>
2025-01-17 14:50:56 -0800

    build: upgrade various packages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,17 @@
 import type { StorybookConfig } from "@storybook/nextjs"
 const config: StorybookConfig = {
   stories: ["../components/**/*.stories.mdx", "../components/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@chromatic-com/storybook"
+  ],
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+  docs: {},
   typescript: {
     check: false,
     checkOptions: {},
